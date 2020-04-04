5a8b7fc4018484144559f9b5b874a67b7ed6fe30
Jen McQuade <jenmcquade@gmail.com>
2020-04-04 13:35:13 -0700

    Add sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    student = Student.find_by(email: params[:sessions][:email])
+    if student && student.authenticate(params[:sessions][:password])
+      session[:student_id] = student.id
+      flash[:notice] = 'You have successfully logged in'
+      redirect_to student
+    else
+      flash.now[:error] = 'Something was wrong with your login information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:student_id] = nil
+    flash[:notice] = 'You have successfully logged out'
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'about', to: 'pages#about'
   get 'courses/new', to: 'courses#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :students, except: [:destroy]
 end
