bb3e3bbaec671e6c01a5fdabeb7cfef7e1926879
Jen McQuade <jenmcquade@gmail.com>
2020-04-04 14:55:31 -0700

    Add enrollment functionality

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
+  has_many :student_courses
+  has_many :courses, through: :student_courses
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { minimum: 5, maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-a]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
