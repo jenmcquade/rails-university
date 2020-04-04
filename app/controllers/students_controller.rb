0692f70b0d7690461c439af3bd2c36eb226a9146
Jen McQuade <jenmcquade@gmail.com>
2020-04-04 12:05:19 -0700

    View and Controller redundancy extraction

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,11 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
+  
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -15,13 +16,29 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = 'You have successfully signed up'
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @student.update(student_params)
+      flash[:notice] = 'You have successfully edited your profile'
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
